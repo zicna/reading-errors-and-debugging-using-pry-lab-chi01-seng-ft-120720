4af87d469fe33567195389ee9884647654d4ba78
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-16 08:03:18 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,7 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def get_user_input
   gets.chomp
 end
@@ -11,9 +13,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if (string[0] == "s")
+    "ssssssssss" + string
   else
     string
   end
