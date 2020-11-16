09b6ca67d3dbbc354d5ee6d3967a3878362353cc
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-16 07:54:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -6,6 +6,6 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2, player1)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
